b67a3afa9aa7bae6c1aa06e5a25ec8a20635f20e
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-04-13 03:24:24 +0100

    setup common package in app

--- packages/app/android/app/src/main/java/com/gamifiedreactnative/MainActivity.java
+++ packages/app/android/app/src/main/java/com/gamifiedreactnative/MainActivity.java
@@ -1,4 +1,4 @@
-package com.gamifiedlearningapp;
+package com.gamifiedreactnative;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "GamifiedLearningApp";
+    return "GamifiedReactNative";
   }
 }
--- packages/app/android/app/src/main/java/com/gamifiedreactnative/MainApplication.java
+++ packages/app/android/app/src/main/java/com/gamifiedreactnative/MainApplication.java
@@ -1,4 +1,4 @@
-package com.gamifiedlearningapp;
+package com.gamifiedreactnative;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.gamifiedlearningapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.gamifiedreactnative.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
